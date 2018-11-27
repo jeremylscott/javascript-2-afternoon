2414ec6c2c1159d75ef8c0d8c81d76b1e8d53e4d
Jeremy Scott <jeremylscott@gmail.com>
2018-11-27 15:15:26 -0600

    First javascript project

--- practice.js
+++ practice.js
@@ -14,7 +14,12 @@
   Then alert your name using dot notation.
 */
 
-//Code here
+let me = { 
+  name: 'Jeremy',
+  age: 42
+}
+
+alert(me.age)
 
 
 
@@ -25,7 +30,14 @@
   Have the values to those keys be your favorite thing in that category.
 */
 
-//Code here
+let favoriteThings = { 
+  band: 'pearljam',
+  food: 'chicken fried steak',
+  person: 'wife',
+  book: 'faith of the fallen',
+  movie: 'braveheart',
+  holiday: 'christmas'
+}
 
 
 
@@ -33,7 +45,8 @@
   After you've made your object, add another key named 'car' with the value being your favorite car and then another key named 'brand' with the value being your favorite brand.
 */
 
-//Code here
+favoriteThings.car = 'charger'
+favoriteThings.brand = 'noname'
 
 
 
@@ -41,7 +54,9 @@
   Now change the value of the food key in your favoriteThings object to be 'Chicken Nuggets' and change the value of the book key in your favoriteThings object to be 'Harry Potter'.
 */
 
-//Code here
+favoriteThings.food = 'Chicken Nuggets'
+favoriteThings.book = 'Harry Potter'
+
 
 
 
@@ -55,7 +70,13 @@
   Using dot notation, add another key (or property) to your backPack object that is named color, with the value being the color of your backpack. 
 */
 
-//Code here
+let backPack = {}
+
+let item = 'firstPocket'
+
+backPack[item] = 'chapstick'
+
+backPack.color = 'black'
 
 
 
@@ -63,7 +84,7 @@
   After you do the above, alert your entire backPack object.
 */
 
-//Code here
+alert('backPack')
 
 
 
@@ -73,7 +94,7 @@ Alerting to see the data in your Object doesn't work so well.
 Instead, console.log your whole backPack object and then check out the console. 
 */
 
-//Code here
+console.log(backPack);
 
 
 
@@ -96,10 +117,8 @@ var user2 = {
   Make that change without modifying the original object code above.
 */
 
-//Code Here
-
-
-
+user2.name = `Tyler S. McGinnis`
+user2.email = `tyler.mcginnis@devmounta.in`
 
 
 
